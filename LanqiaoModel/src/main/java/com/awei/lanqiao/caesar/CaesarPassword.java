8d907bed42f5ec48e4022063a8831f8c80b03c6c
shizuwei <shizuwei99@gmail.com>
2021-03-29 09:03:44 +0800

    CaesarPassword

---
+++ LanqiaoModel/src/main/java/com/awei/lanqiao/caesar/CaesarPassword.java
@@ -0,0 +1,24 @@
+package com.awei.lanqiao.caesar;
+
+/**
+ * @Description: 凯撒密码
+ * @Author: Awei
+ * @Create: 2021-03-29 08:10
+ **/
+public class CaesarPassword {
+    public static void main(String[] args) {
+        String str = "xyzabc";
+        char[] chs = str.toCharArray();
+        for (int i = 0; i < chs.length; i++) {
+            if (chs[i] < 'x') {
+                chs[i] += 3;
+            } else chs[i] -= 23;
+        }
+        StringBuffer out = new StringBuffer();
+        for (int i = 0; i < chs.length; i++) {
+            out.append(chs[i] + "--");
+        }
+        System.out.println(out.substring(0, out.length() - 2));
+
+    }
+}
